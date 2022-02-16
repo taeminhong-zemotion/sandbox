33360ab89bca53b92f32d891a6262cec3812f617
Taemin Hong <taemin.hong@z-emotion.com>
2022-02-16 15:25:47 +0900

    Add post-build script

---
+++ pack.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+IFS='
+'
+USAGE="$(printf "%s [--prefix|-p=<dir>]" "$(basename "$0")")"
+PREFIX=
+
+die () {
+	die_with_status 1 "$@"
+}
+
+die_with_status () {
+	status="$1"
+	shift
+	printf >&2 '%s\n' "$*"
+	exit "$status"
+}
+
+usage () {
+	die "usage: $USAGE"
+}
+
+while [ $# != 0 ] && [ -z "$command" ]
+do
+	case "$1" in
+    --prefix|-p)
+        shift
+        PREFIX="$1"
+        ;;
+    --prefix=*|-p=*)
+        PREFIX="${1#*=}"
+        ;;
+    --)
+        shift
+        break
+        ;;
+    -*)
+        usage
+        ;;
+    esac
+    shift
+done
+
+find . -path ./.git -prune -o -type f -exec sha1sum {} + | while read -r line
+    do
+        hash="${line:0:40}"
+        source="${line:42}"
+        ( [ -z "$hash" ] || [ -z "$source" ] ) && die "invalid input: $line"
+        target="${PREFIX:+"$PREFIX/"}${hash:0:2}/${hash}"
+        if [ ! -e "$target" ]
+        then
+            mkdir -p "$(dirname "$target")" && cp -a "$source" "$target"
+        fi
+    done

5415cb8350279697a90924ee631d1e9ff3584c51
Taemin Hong <taemin.hong@z-emotion.com>
2022-02-16 16:26:26 +0900

    pack.sh: let user specify the root directory to copy
    wip

--- pack.sh
+++ pack.sh
@@ -2,8 +2,7 @@
 
 IFS='
 '
-USAGE="$(printf "%s [--prefix|-p=<dir>]" "$(basename "$0")")"
-PREFIX=
+USAGE="$(printf "%s SOURCE DEST" "$(basename "$0")")"
 
 die () {
 	die_with_status 1 "$@"
@@ -20,35 +19,29 @@ usage () {
 	die "usage: $USAGE"
 }
 
-while [ $# != 0 ]
-do
-	case "$1" in
-    --prefix|-p)
-        shift
-        PREFIX="$1"
-        ;;
-    --prefix=*|-p=*)
-        PREFIX="${1#*=}"
-        ;;
-    --)
-        shift
-        break
-        ;;
-    -*)
-        usage
-        ;;
-    esac
-    shift
-done
+if [ $# -lt 2 ]
+then
+    usage
+fi
+
+if [ ! -d "$1" ]
+then
+    die "no such directory: $1"
+fi
+
+DEST=$(realpath "${2:-.}")
+pushd "${1:-.}" >/dev/null
 
 find . -path ./.git -prune -o -type f -exec sha1sum {} + | while read -r line
     do
         hash="${line:0:40}"
-        source="${line:42}"
-        ( [ -z "$hash" ] || [ -z "$source" ] ) && die "invalid input: $line"
-        target="${PREFIX:+"$PREFIX/"}${hash:0:2}/${hash}"
-        if [ ! -e "$target" ]
+        from="${line:42}"
+        to="$DEST/${hash:0:2}/${hash}"
+        if [ ! -e "$to" ]
         then
-            mkdir -p "$(dirname "$target")" && cp -a "$source" "$target"
+            mkdir -p "$(dirname "$to")" && cp -a "$from" "$to" || exit 1
         fi
+        echo "$line"
     done
+
+popd >/dev/null

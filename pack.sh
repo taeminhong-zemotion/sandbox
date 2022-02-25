326f5d7d5a65b3688904f325b690995a736c108f
Taemin Hong <taemin.hong@z-emotion.com>
2022-02-25 16:09:12 +0900

    pack.sh: privide a way to give find's options

--- pack.sh
+++ pack.sh
@@ -9,6 +9,21 @@ then
     usage
 fi
 
+find_opts=()
+
+while [ $# != 0 ]
+do
+    case "$1" in
+    --* | -*)
+        find_opts+=("$1")
+        shift
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
 if [ ! -d "$1" ]
 then
     die "no such directory: $1"
@@ -17,7 +32,7 @@ fi
 DEST=$(realpath "${2:-.}")
 pushd "${1:-.}" >/dev/null
 
-find . -path ./.git -prune -o -type f -exec sha1sum {} + | while read -r line
+find "${find_opts[@]}" . -path ./.git -prune -o -type f -exec sha1sum {} + | while read -r line
     do
         hash="${line:0:40}"
         from="${line:42}"
